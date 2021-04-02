f571e7443b3987c32ca6cfe12aafd42bc9f9aa1a
Nachapol <nachapol.s@kkumail.com>
2021-04-02 11:01:08 +0700

    update by earthy (02/04/64)

--- ProjectACP/src/projectacp/MainPage.java
+++ ProjectACP/src/projectacp/MainPage.java
@@ -190,7 +190,8 @@ public class MainPage extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton5ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        
+        showRegularly showingWindow = new showRegularly("Show  activities");
+        showingWindow.createAndShowGUI();        
     }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
